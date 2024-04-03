62ba068757faf3a8803515dbd977268bee69a7d0
Gokula94 <mailgokul94@gmail.com>
2024-04-03 21:28:19 +0100

    patch6

--- internal/controller/post_controller.go
+++ internal/controller/post_controller.go
@@ -64,15 +64,16 @@ func (r *PostReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.
 	// Call REST API
 	//myurl := "https://api.restful-api.dev/objects"
 	post := &httpv1alpha1.Post{}
+	logger.Info("evaluating custom resource")
 
-	if err := r.Get(ctx, req.NamespacedName, post); err != nil {
-		logger.Info("no resource found")
-	}
-	logger.Info(fmt.Sprintf("Pod created is %v", req.NamespacedName))
+	err := r.Get(ctx, req.NamespacedName, post)
+	if err == nil {
 
-	const myurl = "https://api.restful-api.dev/objects"
-	fmt.Println(myurl)
-	requestBody := strings.NewReader(`
+		//logger.Info(fmt.Sprintf("Pod created is %v", req.NamespacedName))
+
+		const myurl = "https://api.restful-api.dev/objects"
+		fmt.Println(myurl)
+		requestBody := strings.NewReader(`
 			 
 				{
 					"name": "3801-XGS-PON",
@@ -83,16 +84,17 @@ func (r *PostReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.
 				 }
 
 			 `)
-	fmt.Println(requestBody)
-	response, err := http.Post(myurl, "application/json", requestBody)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("post call is suceesful")
-	defer response.Body.Close()
-	content, _ := io.ReadAll(response.Body)
+		fmt.Println(requestBody)
+		response, err := http.Post(myurl, "application/json", requestBody)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("post call is suceesful")
+		defer response.Body.Close()
+		content, _ := io.ReadAll(response.Body)
 
-	fmt.Println(string(content))
+		fmt.Println(string(content))
+	}
 
 	// //req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(payload))
 	// req, err := http.Post(myurl, "application/json", payload)
